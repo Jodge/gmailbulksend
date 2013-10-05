6416062eaf09828e9d8d2fca44e16fcbe5a7973d
Jodge <georgeorti@gmail.com>
2013-10-05 18:34:32 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// client id
+$client_id = 'your google client id';
+
+// redirect Url
+$redirectUri = 'your redirect Uri should match the one registered in the api console';
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>Gmail - Send Mail</title>
+   </head>
+<body>
+     <a href="https://accounts.google.com/o/oauth2/auth?client_id=<?php echo $client_id; ?>&redirect_uri=<?php print $redirectUri; ?>&scope=https://www.google.com/m8/feeds/&response_type=code" class="btn btn-danger">Invite Friends</a>
+</body>
+</html>
